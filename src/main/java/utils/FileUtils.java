44826b71b16595506797006f0bf18ddbaf05c957
agherasimov <ala.gherasimov@endava.com>
2019-10-03 11:27:01 +0300

    implementing first task out of 24

--- src/main/java/utils/FileUtils.java
+++ src/main/java/utils/FileUtils.java
@@ -16,7 +16,7 @@ public class FileUtils {
         FileReader fileReader = new FileReader(file);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-
+        //saving each line from input file into a list
         while((line = bufferedReader.readLine()) != null){
             inputStringList.add(line);
         }
@@ -24,21 +24,15 @@ public class FileUtils {
         bufferedReader.close();
 
         return inputStringList;
-
-
     }
+
 
     public static void writeToTxt(String className, List<String> messages) throws IOException {
         File file = new File(Paths.get(String.format("src/main/resources/logs/%s_%s.txt", className, LocalDate.now().toString())).toUri());
-        if (file.createNewFile()) {
-            System.out.println("File is created!");
-        } else {
-            System.out.println("File already exists.");
-        }
         FileWriter writer = new FileWriter(file);
-
         BufferedWriter bufferedWriter = new BufferedWriter(writer);
 
+        //writting results in a txt file
         for(int i = 0; i < messages.size(); i++) {
             bufferedWriter.write(messages.get(i));
             bufferedWriter.newLine();
